d20df40dc789c64da3b44c8055e24b3f0b52d8a7
Sumair Baloch <Sumair Baloch>
2016-07-21 20:02:53 +0500

    withBootStrap

--- Forums/main.js
+++ Forums/main.js
@@ -2,18 +2,29 @@
 var currentTopic;
 var topicRef = firebase.database().ref('/topics/');
 topicRef.on('child_added',function(snapshot){
-    $('#container').append("<a href='topic.html#key="+snapshot.key+"'id='"+snapshot.key+"'>" + snapshot.val().title + " topic started by " + snapshot.val().username +  "</a> <br>");
+    // $('#TopicTable tbody').append("<a href='topic.html#key="+snapshot.key+"'id='"+snapshot.key+"'>" + snapshot.val().title + " topic started by " + snapshot.val().username +  "</a> <br>");
+      $('#tableid tbody').append("<tr id="+ snapshot.key +"><td>"+snapshot.val().title + "</td><td>" + snapshot.val().username+
+      "</td><td>"+ snapshot.val().time +"</td><td id='del"+snapshot.key+"' class='delete glyphicon glyphicon-remove'></tr>");
 
     $('#'+snapshot.key).on('click',function(){
-        currentTopic = snapshot.key;
+        window.location = "topic.html#key="+snapshot.key;
+    });
+
+    $('#del'+snapshot.key).on('click',function(e){
+        e.stopPropagation();
+        var delTopic = firebase.database().ref('/topics/'+snapshot.key);
+        delTopic.remove();
+        $('#'+snapshot.key).remove();
     });
 });
 
 $('#submit').on('click',function(){
+    var date = new Date();
+
     var obj = {
         "title" : $('#topic').val(),
         "username" : $('#user').val(),
-        "time" : Date.now(),
+        "time" : date.getDate() + " " + date.getMonth() + " " + date.getFullYear(),
         "comments" : {
 
         }
@@ -21,4 +32,6 @@ $('#submit').on('click',function(){
 
     topicRef.push(obj);
 }); 
+
+
 
